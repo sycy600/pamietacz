a2be6e192c7aebf4c198ed102f0c9e1b5669d533
sycy600 <sycy600@gmail.com>
2013-09-06 23:30:16 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pamietacz",
-    version = "0.1.0",
+    version = "0.2.0",
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     install_requires = ['setuptools', 'markdown==2.3.1', 'pygments==1.6', 'lxml', 'Pillow'],
