26c5076d3aea029118265e98bf961c5c87b871ec
sycy600 <sycy600@gmail.com>
2013-11-08 23:45:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version = "0.2.0",
     packages = find_packages('src'),
     package_dir = {'': 'src'},
-    install_requires = ['setuptools', 'markdown==2.3.1', 'pygments==1.6', 'lxml', 'Pillow'],
+    install_requires = ['setuptools', 'markdown==2.3.1', 'pygments==1.6', 'lxml', 'Pillow', 'django==1.6.0'],
 )
